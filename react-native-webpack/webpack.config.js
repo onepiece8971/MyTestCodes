67688ef0b617c98288db289e408348c207c598b0
chenglinz <onepiece8971@163.com>
2016-08-15 16:21:41 +0800

    +getReactNativeExternals

--- react-native-webpack/webpack.config.js
+++ react-native-webpack/webpack.config.js
@@ -1,6 +1,14 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// 另一种生成externals->react-native的方式,支持多个platform
+/*var getReactNativeExternals = require('./getReactNativeExternals');
+var reactNativeExternalsPromise = getReactNativeExternals({
+    projectRoots: [__dirname],
+    assetRoots: [__dirname],
+    platforms: ['android', 'ios']
+});*/
+
 var reactNativeExternalsPromise = (function () {
     //var reactNativeRoot = path.dirname(require.resolve('react-native/package'));
     var blacklist = require('react-native/packager/blacklist');
@@ -39,7 +47,7 @@ module.exports = {
             reactNativeExternalsPromise.then(function (reactNativeExternals) {
                 if (['react-native'].concat(reactNativeExternals).indexOf(request) != -1) {
                     cb(null, request);
-                } else{
+                } else {
                     cb();
                 }
             });
