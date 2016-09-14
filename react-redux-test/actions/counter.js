0e3e23c652d9639adf01a354dc95f95f6a53da0d
chenglinz <onepiece8971@163.com>
2016-09-15 00:46:35 +0800

    优化redux,加入规范action配置实例

--- react-redux-test/actions/counter.js
+++ react-redux-test/actions/counter.js
@@ -1,7 +1,8 @@
 import { createActions } from 'redux-actions';
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 // 命名必须要与后面常量保持一致
 export const { incrementCounter, incrementAccount, incrementSub } = createActions(
-  'INCREMENT_COUNTER',
+  INCREMENT_COUNTER,
   'INCREMENT_ACCOUNT',
   'INCREMENT_SUB'
 );
--- react-redux-test/reducers/counter.js
+++ react-redux-test/reducers/counter.js
@@ -1,6 +1,7 @@
 import { handleActions } from 'redux-actions';
+import { INCREMENT_COUNTER } from '../actions/counter';
 const counter = handleActions({
-  INCREMENT_COUNTER: (state, {payload}) => state + parseInt(payload),
+  [INCREMENT_COUNTER]: (state, {payload}) => state + parseInt(payload),
   INCREMENT_SUB: (state, {payload}) => state - parseInt(payload),
 }, 0);
 
